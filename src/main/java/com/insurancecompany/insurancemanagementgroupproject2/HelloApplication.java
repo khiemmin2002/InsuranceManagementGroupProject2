5e6ca47f8dededa08bbae4bab5c8655044273791
Truong Adam Nhat Anh <trongduong04112003@gmail.com>
2024-05-06 10:02:28 +0700

    Setup all front-end for surveyors, moved claim data to List for better handling.

--- src/main/java/com/insurancecompany/insurancemanagementgroupproject2/HelloApplication.java
+++ src/main/java/com/insurancecompany/insurancemanagementgroupproject2/HelloApplication.java
@@ -14,12 +14,12 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/surveyor-homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Surveyor Homepage");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        SurveyorHomepage surveyorHomepage = new SurveyorHomepage();
         launch();
     }
 }
--- src/main/java/com/insurancecompany/insurancemanagementgroupproject2/controller/SurveyorHomepage.java
+++ src/main/java/com/insurancecompany/insurancemanagementgroupproject2/controller/SurveyorHomepage.java
@@ -7,21 +7,30 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class SurveyorHomepage {
     @FXML
     public TableView<Claim> claimTable;
+    @FXML
     public Button fetchAllClaimButton;
+    @FXML
     public Button fetchProposableClaimButton;
+    @FXML
+    public Button fetchSingleClaimButton;
+    @FXML
+    public Label errorLabel;
+    @FXML
+    public TextField insertID;
     @FXML
     private TableColumn<?, ?> claimID;
     @FXML
@@ -42,6 +51,7 @@ public class SurveyorHomepage {
     private TableColumn<?, ?> insuredPerson;
     @FXML
     private TableColumn<?, ?> status;
+    private List<Claim> claimList;
     @FXML
     private void initialize() {
         claimTable.widthProperty().addListener((observable, oldValue, newValue) -> {
@@ -69,30 +79,32 @@ public class SurveyorHomepage {
             bankNumber.setCellValueFactory(new PropertyValueFactory<>("bankNumber"));
             
             fetchAllClaimButton.setOnAction(fetchAllClick);
-            fetchProposableClaimButton.setOnAction(fetchProposableClick);
+            fetchProposableClaimButton.setOnAction(fetchProposalClick);
+            fetchSingleClaimButton.setOnAction(fetchSingleClaimClick);
+            fetchClaimData();
         });
     }
 
-    EventHandler<ActionEvent> fetchAllClick = new EventHandler<ActionEvent>() {
+    EventHandler<ActionEvent> fetchAllClick = _ -> fetchAllClaimData();
+
+    EventHandler<ActionEvent> fetchProposalClick = _ -> fetchStatusNewClaimData();
+
+    EventHandler<ActionEvent> fetchSingleClaimClick = new EventHandler<>() {
         @Override
         public void handle(ActionEvent actionEvent) {
-            fetchAllClaimData();
+            if (insertID.getText().isEmpty()) {
+                errorLabel.setText("Cannot search empty field!");
+            } else {
+                fetchSingleClaim(insertID.getText());
+            }
         }
     };
-
-    EventHandler<ActionEvent> fetchProposableClick = new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent actionEvent) {
-            fetchNewClaimData();
-        }
-    };
-
-    public void fetchAllClaimData() {
+    public void fetchClaimData() {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         Connection connection = databaseConnection.getConnection();
 
         ObservableList<Claim> claimData = FXCollections.observableArrayList();
-
+        claimList = new ArrayList<Claim>();
         try {
             String getClaimsQuery = "SELECT * FROM claims";
             Statement statement = connection.createStatement();
@@ -111,43 +123,57 @@ public class SurveyorHomepage {
                 claim.setBankUserName(queryResult.getString("bank_user_name"));
                 claim.setBankNumber(queryResult.getString("bank_number"));
                 claimData.add(claim);
+                claimList.add(claim);
             }
 //            System.out.println(claimData);
-            claimTable.setItems((ObservableList<Claim>) claimData);
+            claimTable.setItems(claimData);
         } catch (SQLException e) {
             System.out.println("SQL error: " + e);
         }
     }
-
-    public void fetchNewClaimData() {
-        DatabaseConnection databaseConnection = new DatabaseConnection();
-        Connection connection = databaseConnection.getConnection();
-
+    public void fetchSingleClaim(String claimID){
         ObservableList<Claim> claimData = FXCollections.observableArrayList();
-
-        try {
-            String getClaimsQuery = "SELECT * FROM claims WHERE status = 'NEW'";
-            Statement statement = connection.createStatement();
-            ResultSet queryResult = statement.executeQuery(getClaimsQuery);
-
-            while (queryResult.next()) {
-                Claim claim = new Claim();
-                claim.setId(queryResult.getString("claim_id"));
-                claim.setInsuredPerson(queryResult.getString("insured_person"));
-                claim.setCardNumber(queryResult.getString("card_number"));
-                claim.setExamDate(queryResult.getDate("exam_date"));
-                claim.setClaimDate(queryResult.getDate("claim_date"));
-                claim.setClaimAmount(queryResult.getFloat("claim_amount"));
-                claim.setStatus(queryResult.getString("status"));
-                claim.setBankName(queryResult.getString("bank_name"));
-                claim.setBankUserName(queryResult.getString("bank_user_name"));
-                claim.setBankNumber(queryResult.getString("bank_number"));
+        boolean statusCheck = false;
+        for(Claim claim : claimList){
+            if(claim.getId().equals(claimID)){
+                claimData.add(claim);
+                if(claim.getStatus().equals("NEW")){
+                    statusCheck = true;
+                }
+                break;
+            }
+        }
+        claimTable.setItems(claimData);
+        if(statusCheck){createProposeAlert(claimID);
+        }
+    }
+    public void fetchAllClaimData() {
+        ObservableList<Claim> claimData = FXCollections.observableArrayList();
+        claimData.addAll(claimList);
+        claimTable.setItems(claimData);
+    }
+    public void fetchStatusNewClaimData() {
+        ObservableList<Claim> claimData = FXCollections.observableArrayList();
+        for(Claim claim : claimList){
+            if(claim.getStatus().equals("NEW")){
                 claimData.add(claim);
             }
-//            System.out.println(claimData);
-            claimTable.setItems((ObservableList<Claim>) claimData);
-        } catch (SQLException e) {
-            System.out.println("SQL error: " + e);
+        }
+        claimTable.setItems(claimData);
+    }
+    public void createProposeAlert(String claimID){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Propose dialog");
+        alert.setContentText("Proposing claim " + claimID + " to manager?");
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            System.out.println("Claim " + claimID + " proposed successfully!");
+            insertID.setText("");
+            fetchClaimData();
+        } else {
+            insertID.setText("");
+            System.out.println("Claim proposing cancelled.");
         }
     }
 }
