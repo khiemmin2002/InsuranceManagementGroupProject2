d47b549b5adec3185646790929f373832e461f4c
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-05-10 22:27:14 +0700

    Add update claim UI

---
+++ src/main/java/com/insurancecompany/insurancemanagementgroupproject2/controller/UpdateClaimController.java
@@ -0,0 +1,33 @@
+package com.insurancecompany.insurancemanagementgroupproject2.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class UpdateClaimController {
+
+    @FXML
+    private Button cancelUpdateBtn;
+
+    @FXML
+    private Button confirmUpdateBtn;
+
+    @FXML
+    private TextField updateBankNameField;
+
+    @FXML
+    private TextField updateBankNumberField;
+
+    @FXML
+    private TextField updateBankUserNameField;
+
+    @FXML
+    private TextField updateClaimAmountField;
+
+    @FXML
+    private TextField updateClaimIDField;
+
+    @FXML
+    private TextField updateInsuredPersonIDField;
+
+}
