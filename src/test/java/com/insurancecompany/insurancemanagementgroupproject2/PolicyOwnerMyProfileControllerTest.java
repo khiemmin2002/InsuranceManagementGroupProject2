8dc05e0880ad0a0c60729887a9d1324a3ff46f14
Giang Trong Duong <trongduong04112003@gmail.com>
2024-05-15 13:03:44 +0700

    Unit Test

--- src/test/java/com/insurancecompany/insurancemanagementgroupproject2/PolicyOwnerMyProfileControllerTest.java
+++ src/test/java/com/insurancecompany/insurancemanagementgroupproject2/PolicyOwnerMyProfileControllerTest.java
@@ -19,9 +19,6 @@ import static org.mockito.Mockito.*;
 
 class PolicyOwnerMyProfileControllerTest {
     @Mock
-    private DatabaseConnection databaseConnection;
-
-    @Mock
     private Connection connection;
     @Mock
     private ResultSet resultSet;
